798cd75c0098fe45cbca6ac69dca5e4dbca7d0a8
Rafi Maisshadiq <74971414+rmaisshadiq@users.noreply.github.com>
2026-04-21 13:35:20 +0700

    feat: add Navbar component and integrate dayjs for date formatting; update package dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "#components/Navbar";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Welcome to my MacFolio</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
